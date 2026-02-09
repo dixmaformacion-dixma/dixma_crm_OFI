a2760f4fc455de1d55bae352774502b1c057dbe2
Marcos Antonio González Flores <marcosantoniogonzalezflores@gmail.com>
2026-02-09 11:47:36 +0100

    administracionbuscaralumno con più alunni visualizzati nei risultati di ricerca (stessi risultati di tutoriabuscaralumno)

--- administracion_buscarAlumno.php
+++ administracion_buscarAlumno.php
@@ -22,8 +22,9 @@
         if(empty($_GET['valor'])){
             echo "<div class='alert alert-danger' role='alert'> El campo de busqueda no puede estar vacio </div>";
         } else {
-            if($alumno = buscarAlumno($_GET['valor'])){
-
+            // Cambia buscarAlumno() con buscarAlumnos() per ottenere array di risultati
+            if($alumnos = buscarAlumnos($_GET['valor'])){
+                // $alumnos ora contiene un array (vuoto o con più risultati)
             } else {
 
                 echo "<div class='alert alert-danger' role='alert'>No se encuentra ningun alumno</div>";
@@ -92,7 +93,7 @@
 
                 <?php
 
-                    if(!empty($alumno)){
+                    if(!empty($alumnos)){
                         echo "<script>";
                         echo "$('#formBusqueda').remove();";
                         echo "</script>";
@@ -101,7 +102,10 @@
 
                         echo "<h2 class='text-center mt-2 pt-2 pb-2 border border-5' style='background-color: #b0d588; letter-spacing: 7px;'>FICHA DE ALUMNO</h2>";
 
-                        require("template-parts/components/alumno.buscarAlumno.php");
+                        // Itera sui risultati con foreach (come in tutoria_buscarAlumno.php)
+                        foreach($alumnos as $alumno){
+                            require("template-parts/components/alumno.buscarAlumno.php");
+                        }
 
                     }
 
